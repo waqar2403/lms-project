7a9e7259a6cfc3722967c2a2aedc1073b025a594
Waqar <waqar216447@gmail.com>
2025-01-29 09:39:56 +0500

    -

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -4,3 +4,16 @@ import jwt from "jsonwebtoken";
 import { Request,Response,NextFunction } from "express";
 import ErrorHandler from "./ErrorHandler";
 import { redis} from "./redis";
+
+interface ITokenOption{
+    expires: Date;
+    maxAge: number;
+    httpOnly:boolean;
+    samSite:'lax'|'strict'|'none' | undefined;
+    secure?:boolean;
+}
+
+export const sendToken = (user:IUser,statusCode:number,res:Response)=>{
+    const accessToken = user.SignAccessToken();
+    const refreshToken = user.SignRefreshToken(); 
+}
