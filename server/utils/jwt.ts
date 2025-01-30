c3ae9db0422de92805a4e50e872e8c9a2918fef5
Waqar <waqar216447@gmail.com>
2025-01-30 12:42:10 +0500

    -

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -9,11 +9,38 @@ interface ITokenOption{
     expires: Date;
     maxAge: number;
     httpOnly:boolean;
-    samSite:'lax'|'strict'|'none' | undefined;
+    sameSite:'lax'|'strict'|'none' | undefined;
     secure?:boolean;
 }
-
+const AccessTokenExpires = parseInt(
+    process.env.ACCESS_TOKEN_EXP || "900",10
+)
+const RefreshTokenExpire = parseInt(
+    process.env.REFRESH_TOKEN_EXP || "604800",10
+)
+export const accessTokenOptions: ITokenOption = {
+    expires : new Date(Date.now()+ AccessTokenExpires*1000),
+    maxAge: AccessTokenExpires*1000,
+    httpOnly:true,
+    sameSite:"lax",
+}
+export const refreshTokenOptions:ITokenOption = {
+    expires: new Date(Date.now() + RefreshTokenExpire*1000),
+    maxAge: RefreshTokenExpire*1000,
+    httpOnly: true,
+    sameSite:"lax",
+}
 export const sendToken = (user:IUser,statusCode:number,res:Response)=>{
     const accessToken = user.SignAccessToken();
     const refreshToken = user.SignRefreshToken(); 
+    redis.set(user._id as any, JSON.stringify(user) as any);
+
+    if(process.env.NODE_ENV === "production"){
+        accessTokenOptions.secure = true;
+    }
+
+    res.cookie("access_token", accessToken,accessTokenOptions);
+    res.cookie("refresh_token",refreshToken,refreshTokenOptions);
+
+    res.status(statusCode).json({success:true,user,accessToken})
 }
